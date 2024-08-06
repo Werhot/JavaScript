2004b31b5db913e702185da695f2485b17b1e453
Oleg <38222238@mail.ru>
2024-08-06 21:32:52 +0700

    gdsgseg

---
+++ script.js
@@ -0,0 +1,25 @@
+        // $(document).ready(function(){
+        //     $("#button1").click(function(event) {
+        //         let divText = $("div").last().text();
+        //         $("div").html("<p>Paragraph</p>");
+        //         $("div").eq(1).text("eq(1) div");
+        //         $("div").last().text(`${divText} last div`);
+        //         $("span").text("<p>Paragraph</p>");
+        //         $("input").val("value1");
+        //         $("div").first().append("<span>Appended span</span>");
+        //         $("div").first().prepend("<span>prepended span</span>");
+        //         $("div").first().after("<span>After span</span>");
+        //     });
+
+        //     $("#removeButton").click(function() {
+        //         $("#container").remove();
+        //     })
+
+        //     $("#emptyButton").click(function() {
+        //         $("div").first().empty();
+        //     })
+
+        //     $("#colorButton").click(function() {
+        //         $("div").toggleClass("colored").css("font-size", "20px");
+        //     })
+        // }); 
